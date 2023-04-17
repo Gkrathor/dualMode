371b27db666426a4eb8c322a03bad15ec08227d2
Gkrathor <gauravkumar8453@gmail.com>
2023-04-17 22:06:11 +0530

    first commit

---
+++ DualMode/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DualModel.Models;
+
+namespace DualModel.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ViewResult Details()
+        {
+            ViewBag.Title = "Student Details Page";
+            ViewBag.Header = "Student Details ";
+
+            Student s1 = new Student()
+            {
+                StudentId = 100, 
+                StudentName = "XYZ",
+                Branch = "CSE"
+            };
+            Address A1 = new Address()
+            {
+                StudentId = 100,
+                City = "Mysore",
+                State = "Karnataka",
+                Country = "India"
+            };
+            StudentDetailsViewModel s2 = new StudentDetailsViewModel
+            {
+                Student = s1,
+                Address = A1,
+                Title = "Student Details",
+                Header = "Student Details Page"
+            };
+            return View(s2);
+        }
+    }
+}
---
+++ DualMode/Models/StudentDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DualModel.Models
+{
+    public class StudentDetailsViewModel
+    {
+        public Student Student { get; set; }
+        public Address Address { get; set; }
+        public string Title { get; set; }
+        public string Header { get; set; }
+    }
+}
